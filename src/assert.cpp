c6630025936a89d5d9f79a14116f2db137f31d60
doodlah <doodlehp@gmail.com>
2016-04-26 17:57:56 +0300

    Fixed compiler warnings on linux

--- src/assert.cpp
+++ src/assert.cpp
@@ -4,6 +4,7 @@
 #include <cstdio>
 #include <csignal>
 #include <cstdlib>
+#include <cinttypes>
 
 namespace hplang
 {
@@ -12,7 +13,6 @@ void break_here()
 {
 #ifdef _WIN32
     // SIGTRAP is only available on POSIX operating systems
-    
     __builtin_trap();
     //exit(0);
 #else
@@ -22,15 +22,14 @@ void break_here()
 
 void Assert(const char *expr, const char *file, s64 line)
 {
-    fprintf(stderr, "\n%s:%lld:1:\n  !!!ASSERT FAILURE\n\n  %s\n\n", file, line, expr);
-    //fprintf(stderr, "\n\n!ASSERT FAILURE\n\n%s:%lld:\n\n  %s\n\n", file, line, expr);
+    fprintf(stderr, "\n%s:%" PRId64 ":1:\n  !!!ASSERT FAILURE\n\n  %s\n\n", file, line, expr);
     fflush(stderr);
     break_here();
 }
 
 void InvalidCodePath(const char *file, s64 line)
 {
-    fprintf(stderr, "\n%s:%lld:1:\n  !INVALID CODE PATH\n\n", file, line);
+    fprintf(stderr, "\n%s:%" PRId64 ":1:\n  !INVALID CODE PATH\n\n", file, line);
     fflush(stderr);
     break_here();
 }
