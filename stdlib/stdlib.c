c58a0efafa1ef8ef6979c5d6dedc2620f51c13b7
duudlah <doodlehp@gmail.com>
2016-05-25 22:44:05 +0300

    Added hello sample; added 'stdlib'

---
+++ stdlib/stdlib.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t  s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+typedef float   f32;
+typedef double  f64;
+
+FILE* hp_get_stdout() { return stdout; }
+FILE* hp_get_stderr() { return stderr; }
+FILE* hp_get_stdin() { return stdin; }
+
+s64 hp_fwrite(FILE *file, s64 size, u8 *data)
+{ return fwrite(data, 1, size, file); }
+
+s64 hp_write(s64 size, u8 *data)
+{ return fwrite(data, 1, size, stdout); }
+
+s64 hp_print_uint(FILE *file, u64 x)
+{ return fprintf(file, "%" PRIu64, x); }
+
+s64 hp_print_int(FILE *file, s64 x)
+{ return fprintf(file, "%" PRId64, x); }
+
+s64 hp_print_f32(FILE *file, f32 x)
+{ return fprintf(file, "%f", x); }
+
+s64 hp_print_f64(FILE *file, f64 x)
+{ return fprintf(file, "%f", x); }
