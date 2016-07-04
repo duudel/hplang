1c79489f0ceb8902e64b1170dbdee7f0d2ab7a44
duudlah <doodlehp@gmail.com>
2016-07-05 01:32:14 +0300

    Some minor improvements to time profiling

--- src/time_profiler.cpp
+++ src/time_profiler.cpp
@@ -21,7 +21,7 @@ struct Profiling_Event
 {
     s64 time;
     const char *name;
-    s64 end_event_index;
+    s64 begin_event_index;
 
     s64 scope_index;
 };
@@ -67,7 +67,7 @@ Timed_Scope::Timed_Scope(const char *name)
     ASSERT(event_index < MAX_PROFILING_EVENTS);
     event_store[event_index].time = CurrentTime();
     event_store[event_index].name = name;
-    event_store[event_index].end_event_index = -1; // is a begin
+    event_store[event_index].begin_event_index = -1; // is a begin
 }
 
 Timed_Scope::~Timed_Scope()
@@ -75,15 +75,17 @@ Timed_Scope::~Timed_Scope()
     s64 end_event_index = next_event++;
     event_store[end_event_index].time = CurrentTime();
     event_store[end_event_index].name = "";
-    event_store[end_event_index].end_event_index = event_index;
+    event_store[end_event_index].begin_event_index = event_index;
 }
 
 
+// TODO(henrik): Implement scoping of events correcly.
 struct Profiling_Scope
 {
     const char *name;
     s64 total_time;
-    s64 sample_count;
+    s32 sample_count;
+    s32 depth; 
 };
 
 static const s64 MAX_SCOPES = 80;
@@ -125,20 +127,22 @@ void CollateProfilingData(Compiler_Context *ctx)
     for (s64 i = 0; i < event_count; i++)
     {
         Profiling_Event &event = event_store[i];
-        if (event.end_event_index == -1)
+        if (event.begin_event_index == -1)
         {
             parent_index = 0;
             s64 index = GetScopeIndex(parent_index, event.name);
             //printf("Got index %d for %s\n", (s32)index, event.name);
             event.scope_index = index;
             scopes[index].name = event.name;
+            if (scopes[index].depth == 0)
+                scopes[index].depth = parent_index_top;
 
             parent_index_stack[parent_index_top++] = parent_index;
             parent_index = index;
         }
         else
         {
-            Profiling_Event begin_ev = event_store[event.end_event_index];
+            Profiling_Event begin_ev = event_store[event.begin_event_index];
             s64 elapsed_time = event.time - begin_ev.time;
             //printf("%s: %" PRId64 "\n", begin_ev.name, elapsed_time);
 
@@ -154,7 +158,10 @@ void CollateProfilingData(Compiler_Context *ctx)
     for (s64 i = 0; i < scope_index; i++)
     {
         Profiling_Scope scope = scopes[i];
-        printf("\t%s: %" PRId64 ": %" PRId64 " us\n",
+        for (s64 p = 0; p < scope.depth; p++)
+            printf("  ");
+
+        printf("%s: %" PRId32 ": %" PRId64 " us\n",
                scope.name, scope.sample_count, scope.total_time);
     }
 }
